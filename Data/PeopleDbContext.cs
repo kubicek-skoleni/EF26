cdacd8ff44dcf788fac108a529039f01815870ed
Kubicek <urza@URMON>
2026-07-20 11:15:10 +0200

    db context + connection

---
+++ Data/PeopleDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace Data;
+public class PeopleDbContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; } 
+
+    public DbSet<Address> Addresses { get; set; }
+
+    public DbSet<Contract> Contracts { get; set; }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=PeopleDb;Trusted_Connection=True;");
+    }
+}
