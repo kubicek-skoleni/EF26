3042fd137729ae5a2d45c79363de67f7c8469a92
Kubicek <urza@URMON>
2026-07-20 10:58:15 +0200

    model + data + Console app

---
+++ Model/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model;
+public class Person
+{
+    public int Id { get; set; }
+
+    [MaxLength(100)]
+    public string FirstName { get; set; }
+
+    [MaxLength(100)]
+    public string LastName { get; set; }
+
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Range(0, 20)]
+    public int ChildrenCount { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+
+    [NotMapped]
+    public int Age { get => DateTime.Now.Year - DateOfBirth.Year; }
+
+    public Address? Address { get; set; }
+
+    public List<Contract> Contracts { get; set; }
+}
