c7cb8796a55cd256c77d834cb23a9c670e0fec09
Kubicek <82667968+kubicek-skoleni@users.noreply.github.com>
2026-07-21 12:07:04 +0200

    connection string env variable

--- ConsoleAppDbFirst/Data/PeopleDbContext.cs
+++ ConsoleAppDbFirst/Data/PeopleDbContext.cs
@@ -11,9 +11,12 @@ public partial class PeopleDbContext : DbContext
     {
     }
 
+    string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=PeopleDb;Trusted_Connection=True;TrustServerCertificate=True;";
+
     public PeopleDbContext(DbContextOptions<PeopleDbContext> options)
         : base(options)
     {
+        connectionString = Environment.GetEnvironmentVariable("CONNECTION") ?? "default";
     }
 
     public virtual DbSet<Address> Addresses { get; set; }
@@ -24,7 +27,7 @@ public partial class PeopleDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=PeopleDb;Trusted_Connection=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer(connectionString);
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
